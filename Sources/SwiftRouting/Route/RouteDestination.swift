f233b3b5a28dcf19958e04638ed194e15f5fcb60
lowki93 <budain.kevin@yahoo.Fr>
2025-02-17 21:45:53 +0100

    RouteDestination documentation

--- Sources/SwiftRouting/Route/RouteDestination.swift
+++ Sources/SwiftRouting/Route/RouteDestination.swift
@@ -7,17 +7,56 @@
 
 import SwiftUI
 
+/// Protocol that associates a `Route` with its corresponding `View`.
+///
+/// `RouteDestination` is a protocol that defines a static method to return the view
+/// corresponding to a given route.
+///
+/// ## Implementing `RouteDestination`
+/// You can directly conform your route to this protocol:
+/// ```swift
+/// extension HomeRoute: @retroactive RouteDestination {
+///   public static func view(for route: HomeRoute) -> some View {
+///     switch route {
+///       case .page1: Page1View()
+///       case let .page2(value): Page2View(value: value)
+///       case let .page3(value): Page3View(value: value)
+///     }
+///   }
+/// }
+/// ```
+///
+/// Alternatively, you can create a separate view if you need to use an environment object:
+/// ```swift
+/// extension HomeRoute: @retroactive RouteDestination {
+///   public static func view(for route: HomeRoute) -> some View {
+///     HomeRouteDestination(route: route)
+///   }
+/// }
+/// ```
 public protocol RouteDestination: Hashable, Identifiable {
   associatedtype R: Route
   associatedtype Destination: View
 
+  /// Return the corect view link to a Route
+  /// ```swift
+  /// extension HomeRoute: @retroactive RouteDestination {
+  ///   public static func view(for route: HomeRoute) -> some View {
+  ///     switch route {
+  ///       case .page1: Page1View()
+  ///       case let .page2(value): Page2View(value: value)
+  ///       case let .page3(value): Page3View(value: value)
+  ///     }
+  ///   }
+  /// }
+  /// ```
   @MainActor @ViewBuilder static func view(for route: R) -> Destination
 }
 
 extension RouteDestination {
   public var id: Int { hashValue }
 
-  @MainActor public static subscript(route: R) -> some View {
+  @MainActor static subscript(route: R) -> some View {
     Self.view(for: route).modifier(LifecycleModifier(route: route))
   }
 }
