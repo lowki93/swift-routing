c5759ab8e8330f29726a3502427c154fac6576fb
kevin.budain <kevin.budain@sorare.com>
2025-02-18 16:01:06 +0100

    doc for RouterType

--- Sources/SwiftRouting/Router/RouterType.swift
+++ Sources/SwiftRouting/Router/RouterType.swift
@@ -5,15 +5,23 @@
 //  Created by Kevin Budain on 06/02/2025.
 //
 
-public enum RouterType: Hashable, Sendable {
+import Foundation
+
+/// Defines the different types of routers available.
+enum RouterType: Hashable, Sendable {
+  /// The primary router instantiated at app launch.
   case app
+  /// A router associated with a specific tab.
   case tab(String)
+  /// A router linked to a navigation stack.
   case stack(String)
+  /// A router presented as a sheet or cover.
   case presented(String)
 
+  /// Indicates whether the router is presented as a sheet or cover.
   var isPresented: Bool {
     switch self {
-    case .presented:  true
+    case .presented: true
     default: false
     }
   }
